f6f030a0414832581886be4e5f41d2e2850f1ebb
Abibubble <abi.yamaha.neo@gmail.com>
2020-12-23 12:24:49 +0000

    feat: Add initial functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,17 @@
 function chooseHouse(obj) {
     let houseChosen = obj.id;
-    document.getElementsByClassName("answer").classList.remove("answer-color");
+    let answers = document.getElementsByClassName("answer")
     if (houseChosen === "gryffindor") {
-        document.getElementsByClassName("answer").classList.add("gryffindor-color");
+        answers.classList.add("gryffindor-color");
     } else if (houseChosen === "slytherin") {
-        document.getElementsByClassName("answer").classList.add("slytherin-color");
+        answers.classList.add("slytherin-color");
     } else if (houseChosen === "hufflepuff") {
-        document.getElementsByClassName("answer").classList.add("hufflepuff-color");
+        answers.classList.add("hufflepuff-color");
     } else if (houseChosen === "ravenclaw") {
-        document.getElementsByClassName("answer").classList.add("ravenclaw-color");
+        answers.classList.add("ravenclaw-color");
     } else {
         alert("Error! House not found. Please try again.");
-    }
+    };
 }
 
 function checkAnswer() {
