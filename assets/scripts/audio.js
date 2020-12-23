dfb3a4d740a5f6f4c79bc2fc15725c80ee3b530b
Abibubble <abi.yamaha.neo@gmail.com>
2020-12-23 12:23:49 +0000

    docs: Add audio js ready for audio files

---
+++ assets/scripts/audio.js
@@ -0,0 +1,15 @@
+let audio = {
+    backgroundAudio: new Audio("assets/audio/background.mp3"),
+
+    correctGryffindorClick: new Audio("assets/audio/gryffindorright.wav"),
+    incorrectGryffindorClick: new Audio("assets/audio/gryffindorwrong.wav"),
+
+    correctSlytherinClick: new Audio("assets/audio/slytherinright.wav"),
+    incorrectSlytherinClick: new Audio("assets/audio/slytherinwrong.wav"),
+
+    correctHufflepuffClick: new Audio("assets/audio/hufflepuffright.wav"),
+    incorrectHufflepuffClick: new Audio("assets/audio/hufflepuffwrong.wav"),
+
+    correctRavenclawClick: new Audio("assets/audio/ravenclawright.wav"),
+    incorrectRavenclawClick: new Audio("assets/audio/ravenclawwrong.wav"),
+};
