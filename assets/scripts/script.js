0309134b82335614425f276019305b7b37dc88b2
Abibubble <abi.yamaha.neo@gmail.com>
2020-12-24 14:09:02 +0000

    maint: Put all scripts into one script file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,9 @@
 // Sets color scheme depending on which House is chosen on index.html
 
+const houseChosen;
+
 function chooseHouse(obj) {
-    const houseChosen = obj.id;
+    houseChosen = obj.id;
 
     console.log(houseChosen); // Gets the correct id - gryffindor
     console.log(this); // Yet this isn't working? - Window, etc.
@@ -9,15 +11,477 @@ function chooseHouse(obj) {
     let answers = document.getElementsByClassName("answer")
     console.log(answers); // HTML collection
 
-    if (houseChosen === "gryffindor") {
+    if (houseChosen === "js-gryffindor") {
         answers.classList.add("gryffindor-color"); // TypeError, cannot read 'add' of undefined
-    } else if (houseChosen === "slytherin") {
+    } else if (houseChosen === "js-slytherin") {
         answers.classList.add("slytherin-color");
-    } else if (houseChosen === "hufflepuff") {
+    } else if (houseChosen === "js-hufflepuff") {
         answers.classList.add("hufflepuff-color");
-    } else if (houseChosen === "ravenclaw") {
+    } else if (houseChosen === "js-ravenclaw") {
         answers.classList.add("ravenclaw-color");
     } else {
         alert("Error! House not found. Please try again.");
     };
+    // Move to next section (hide this one, show next one)
+}
+
+//
+//
+// Quiz questions
+// And functions
+//
+//
+
+// ---------------------------------------------------------------- Questions sets
+
+let gryffindorQuestions = [ 
+  {
+    question: "Which animal represents your house?",
+    optionA: "Lion",
+    optionB: "Snake",
+    optionC: "Badger",
+    optionD: "Eagle",
+    correct: "A",
+  },
+  {
+    question: "What are your house colours? Scarlet + gold, Silver + green, Yellow + black, Blue + silver",
+    optionA: "Scarlet and Gold",
+    optionB: "Silver and Green",
+    optionC: "Yellow and Black",
+    optionD: "Blue and Silver",
+    correct: "A",
+  },
+  {
+    question: "Who was Hermione's date at the Yule Ball?",
+    optionA: "",
+    optionB: "",
+    optionC: "Viktor Krum",
+    optionD: "",
+    correct: "C",
+  },
+  {
+    question: "Which character served as Quiddich commentator?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "Lee Jordan",
+    correct: "D",
+  },
+  {
+    question: "Which insect is Rom most scared of?",
+    optionA: "",
+    optionB: "Spiders",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "What is the name of Hermione's cat?",
+    optionA: "",
+    optionB: "Crookshanks",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "How did Neville help to defeat Voldemort?",
+    optionA: "",
+    optionB: "Killed Nagini",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "How does Harry catch his first snitch?",
+    optionA: "In his mouth",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "A",
+  },
+  {
+    question: "How does Hermione take extra classes in third year?",
+    optionA: "Weekend classes",
+    optionB: "Made a clone",
+    optionC: "Penseive",
+    optionD: "Time Turner",
+    correct: "D",
+  },
+  {
+    question: "Other than Harry, who else could have been the object of the prophecy regarding Voldemort's downfall?",
+    optionA: "Hermione Granger",
+    optionB: "Luna Lovegood",
+    optionC: "Neville Longbottom",
+    optionD: "Dean Thomas",
+    correct: "A",
+  },
+];
+
+let slytherinQuestions = [   
+  {
+    question: "Which animal represents your house?",
+    optionA: "Lion",
+    optionB: "Snake",
+    optionC: "Badger",
+    optionD: "Eagle",
+    correct: "B",
+  },
+  {
+    question: "What are your house colours? Scarlet + gold, Silver + green, Yellow + black, Blue + silver",
+    optionA: "Scarlet and Gold",
+    optionB: "Silver and Green",
+    optionC: "Yellow and Black",
+    optionD: "Blue and Silver",
+    correct: "B",
+  },
+  {
+    question: "How many horcruxes did Voldemort make?",
+    optionA: "",
+    optionB: "8",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "Finish the quote. “After all this time?”",
+    optionA: "Always.",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "A",
+  },
+  {
+    question: "What was the name of Voldemort's snake?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "Nagini",
+    correct: "D",
+  },
+  {
+    question: "Who killed Dobby by throwing a knife at him?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "Bellatrix Lestrange",
+    correct: "D",
+  },
+  {
+    question: "Which medieval wizard was in Slytherin?",
+    optionA: "",
+    optionB: "",
+    optionC: "Merlin",
+    optionD: "",
+    correct: "C",
+  },
+  {
+    question: "Where is the common room?",
+    optionA: "In the dungeons",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "A",
+  },
+  {
+    question: "Who is the resident ghost of Slytherin?",
+    optionA: "Nearly Headless Nick",
+    optionB: "The Bloody Baron",
+    optionC: "The Fat Friar",
+    optionD: "The Grey Lady",
+    correct: "B",
+  },
+  {
+    question: "What qualities does a Slytherin possess?",
+    optionA: "Patience and Loyalty",
+    optionB: "Wit and Learning",
+    optionC: "Cunning and Deceit",
+    optionD: "Daring and Nerve",
+    correct: "C",
+  },
+];
+
+let hufflepuffQuestions = [   
+  {
+    question: "Which animal represents your house?",
+    optionA: "Lion",
+    optionB: "Snake",
+    optionC: "Badger",
+    optionD: "Eagle",
+    correct: "C",
+  },
+  {
+    question: "What are your house colours? Scarlet + gold, Silver + green, Yellow + black, Blue + silver",
+    optionA: "Scarlet and Gold",
+    optionB: "Silver and Green",
+    optionC: "Yellow and Black",
+    optionD: "Blue and Silver",
+    correct: "C",
+  },
+  {
+    question: "What colour ink are Hogwarts invitation letters written in?",
+    optionA: "Red",
+    optionB: "Blue",
+    optionC: "Green",
+    optionD: "Black",
+    correct: "C",
+  },
+  {
+    question: "Who is the resident ghost of Hufflepuff?",
+    optionA: "Nearly Headless Nick",
+    optionB: "The Bloody Baron",
+    optionC: "The Fat Friar",
+    optionD: "The Grey Lady",
+    correct: "C",
+  },
+  {
+    question: "Which Quiddich position did Captain Cedric Diggory play?",
+    optionA: "Keeper",
+    optionB: "Seeker",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "Which dragon did Cedric Diggory face in the Triwizard Tournament?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "D",
+  },
+  {
+    question: "What was the password to the prefect's bathroom?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "Sherbet lemons",
+    correct: "D",
+  },
+  {
+    question: "Where is the Hufflepuff common room?",
+    optionA: "In the dungeons",
+    optionB: "In the kitchen",
+    optionC: "",
+    optionD: "",
+    correct: "B",
+  },
+  {
+    question: "Who is the head of Hufflepuff house?",
+    optionA: "",
+    optionB: "",
+    optionC: "Professor Sprout",
+    optionD: "",
+    correct: "C",
+  },
+  {
+    question: "How do you get into the Hufflepuff common room?",
+    optionA: "Hum a tune",
+    optionB: "Tap out a rhythm",
+    optionC: "Perform a poem",
+    optionD: "Dance",
+    correct: "B",
+  },
+];
+
+let ravenclawQuestions = [   
+  {
+    question: "Which animal represents your house?",
+    optionA: "Lion",
+    optionB: "Snake",
+    optionC: "Badger",
+    optionD: "Eagle",
+    correct: "D",
+  },
+  {
+    question: "What are your house colours? Scarlet + gold, Silver + green, Yellow + black, Blue + silver",
+    optionA: "Scarlet and Gold",
+    optionB: "Silver and Green",
+    optionC: "Yellow and Black",
+    optionD: "Blue and Silver",
+    correct: "D",
+  },
+  {
+    question: "What qualities does a Ravenclaw possess?",
+    optionA: "Patience and Loyalty",
+    optionB: "Wit and Learning",
+    optionC: "Cunning and Deceit",
+    optionD: "Daring and Nerve",
+    correct: "B",
+  },
+  {
+    question: "Which gem is contained in the Ravenclaw house points hourglass?",
+    optionA: "Amethysts",
+    optionB: "Sapphires",
+    optionC: "Emeralds",
+    optionD: "Rubies",
+    correct: "D",
+  },
+  {
+    question: "What does Felix Felicis do? Gives good luck, poisons you, fall in love, resurrection",
+    optionA: "Poisons the consumer",
+    optionB: "Gives the consumer good luck",
+    optionC: "Makes you fall in love",
+    optionD: "Ressurects someone",
+    correct: "B",
+  },
+  {
+    question: "Who is the Seeker of the Ravenclaw Quiddich team?",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "Cho Chang",
+    correct: "D",
+  },
+  {
+    question: "Who is the resident ghost of Ravenclaw?",
+    optionA: "Nearly Headless Nick",
+    optionB: "The Bloody Baron",
+    optionC: "The Fat Friar",
+    optionD: "The Grey Lady",
+    correct: "D",
+  },
+  {
+    question: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "",
+  },
+  {
+    question: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "",
+  },
+  {
+    question: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    correct: "",
+  },
+];
+
+// ---------------------------------------------------------------- Chooses question set depending on which house was chosen
+
+let questionsSet;
+
+function chooseQuestionSet() { // CHECK THAT THIS CHOOSES QUESTION SET BY HOUSE
+    if (houseChosen === "gryffindor") {
+        questionsSet = gryffindorQuestions;
+    } else if (houseChosen === "slytherin") {
+        questionsSet = slytherinQuestions;
+    } else if (houseChosen === "hufflepuff") {
+        questionsSet = hufflepuffQuestions;
+    } else if (houseChosen === "ravenclaw") {
+        questionsSet = ravenclawQuestions;
+    } else {
+        alert("No house chosen. Please choose a house!");
+    }
+    // Move to next scene???
+}
+
+// ---------------------------------------------------------------- Randomises the order of the questions
+
+let currentQuestion;
+
+function randomiseQuestionOrder() {
+    let randomNumber = Math.floor(Math.random() * 10);
+    currentQuestion = questionsSet[randomNumber];
+}
+
+function populateQuestion() {
+    let questionText = document.getElementById("question-text");
+    questionText.innerText = currentQuestion.question;
+
+    let answerOne = document.getElementById("answer-one");
+    answerOne.innerText = currentQuestion.optionA;
+
+    let answerTwo = document.getElementById("answer-two");
+    answerTwo.innerText = currentQuestion.optionB;
+
+    let answerThree = document.getElementById("answer-three");
+    answerThree.innerText = currentQuestion.optionC;
+
+    let answerFour = document.getElementById("answer-four");
+    answerFour.innerText = currentQuestion.optionD;
+}
+
+// ---------------------------------------------------------------- Check the answer given by the user and move to the next question
+
+let score = 0;
+
+function checkAnswer(num) {
+    let currentQuestionResponse; // id of clicked answer
+   if (currentQuestionResponse == currentQuestion.correct) { //if id of clicked answer is equal to the question correct answer
+       score++; // Add to the score
+   };
+   if (currentQuestion < questionsSet.length) { // if the current question isn't the final question
+       currentQuestion++; // move to the next question
+   } else {
+       timerEnd();
+   }
+}
+
+// ---------------------------------------------------------------- Creates the 180 second timer for the full quiz
+
+let timer;
+
+function timer() {
+    // When question page is fully loaded
+    // Start timer
+    // Count down from 180 seconds
+    timerEnd();
+}
+
+function timerEnd() {
+    //If timer runs out, end the quiz
+    showScorePage();
+}
+
+// ---------------------------------------------------------------- Decides which results page to show the user, and displays their score
+
+function showScorePage() {
+    if (score > 5) {
+        // Go to good results page
+        // Populate their score
+    } else {
+        // Go to bad results page
+        // Populate their score
+    }
+}
+
+//
+//
+// Audio settings
+//
+//
+//
+
+let audio = {
+    backgroundAudio: new Audio("assets/audio/background.mp3"),
+
+    correctGryffindorClick: new Audio("assets/audio/gryffindorright.wav"),
+    incorrectGryffindorClick: new Audio("assets/audio/gryffindorwrong.wav"),
+
+    correctSlytherinClick: new Audio("assets/audio/slytherinright.wav"),
+    incorrectSlytherinClick: new Audio("assets/audio/slytherinwrong.wav"),
+
+    correctHufflepuffClick: new Audio("assets/audio/hufflepuffright.wav"),
+    incorrectHufflepuffClick: new Audio("assets/audio/hufflepuffwrong.wav"),
+
+    correctRavenclawClick: new Audio("assets/audio/ravenclawright.wav"),
+    incorrectRavenclawClick: new Audio("assets/audio/ravenclawwrong.wav"),
+};
+
+function backgroundMusicToggle() {
+
+}
+
+function soundEffectsToggle() {
+    
 }
